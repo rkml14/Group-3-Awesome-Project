76e7781d02f928b0c8d8d16392e5d8aa2adb2f6c
Big Bob <devon.eadie@yahoo.com>
2023-03-01 11:17:01 -0800

    connected login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -46,5 +46,15 @@ router.get('/myprofile', withAuth, async (req, res) => {
   }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/myprofile');
+    return;
+  }
+
+  res.render('login');
+});
+
 
 module.exports = router;
